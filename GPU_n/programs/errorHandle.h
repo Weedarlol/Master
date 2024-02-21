db11bf80de3fe7fa0663542f5462a59584260187
Vidar Lunde Olaisen <vidaro@srl-login1.cm.cluster>
2024-02-21 17:07:21 +0100

    Tried to tidy up the GPU_n folder

---
+++ GPU_n/programs/errorHandle.h
@@ -0,0 +1,16 @@
+#ifndef ERROR_HANDLE_H
+#define ERROR_HANDLE_H
+
+#include <cuda_runtime.h>
+
+#define cudaErrorHandle(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort)
+            exit(code);
+    }
+}
+
+#endif
