57f9f39b39faa87d7f2295628d13e3cb8746149b
ec-vidarlol <ec-vidarlol@login-1.fox.ad.fp.educloud.no>
2024-02-22 10:31:59 +0100

    Updated Multinode

--- Multi_Node/programs/jacobi.cu
+++ Multi_Node/programs/jacobi.cu
@@ -5,66 +5,75 @@
 
 namespace cg = cooperative_groups;
 
-__device__ void calc(double *mat_gpu, double *mat_gpu_tmp, int amountPerThread, int index_start, int jacobiSize, int width, int height, double eps, int *maxEps, int thread, cg::grid_group grid_g){
-    int local_var = 0;
-
-    for(int i = 0; i < amountPerThread; i++){
-        int index = index_start + i;
+__device__ void calc(double *mat_gpu, double *mat_gpu_tmp, int elementsPerThread, int index_start, int width, int height, int thread, cg::grid_group grid_g, int threadSize){
+    for(int i = 0; i < elementsPerThread; i++){
+        int index = index_start + i*threadSize;
         int x = index % (width-2) + 1;
         int y = index / (width-2) + 1;
         index = x + y*width;
         mat_gpu_tmp[index] = 0.25 * (
             mat_gpu[index + 1]     + mat_gpu[index - 1] +
             mat_gpu[index + width] + mat_gpu[index - width]);
-
-        if(abs(mat_gpu[index] - mat_gpu_tmp[index]) > eps){
-            local_var++;
-        }
-    }
-
-     // https://developer.nvidia.com/blog/cooperative-groups/
-    for (int i = grid_g.num_threads() / 2; i > 0; i /= 2){
-        maxEps[thread] = local_var;
-        grid_g.sync(); // wait for all threads to store
-        if(thread<i) local_var += maxEps[thread + i];
-        grid_g.sync(); // wait for all threads to load
-    }
+    }   
 }
 
-__global__ void jacobi(double *mat_gpu, double *mat_gpu_tmp, int number_rows, int width, int height, 
-                        int rows_leftover, int device_nr, int rows_compute, int amountPerThreadExtra, int leftoverExtra,
-                        int amountPerThread, int leftover, int *maxEps, double eps){
-    /*
-    Variables      | Type      | Description
-    grid_g         | grid_group| Creates a group compromising of all the threads
-    thread         | int       | The index of each thread
-    index_start    | int       | Element index the thread will start computing on, unique for each thread in grid_g group. It is also taking into consideration what device is computing
-    */
-
-   // number_rows -> Totalt antall allokert på GPU
-   // rows_leftover -> Antall 
+__global__ void jacobiEdge(double *mat_gpu, double *mat_gpu_tmp, int width, int height, 
+                        int rows_compute, int elementsPerThread, int elementsLeftover,
+                        int warpAmount){
 
     cg::grid_group grid_g = cg::this_grid();
     int thread = grid_g.thread_rank();
-    int index_start;
+    int threadSize = grid_g.size();
 
-    
-    if(device_nr < rows_leftover){
-        index_start = thread*amountPerThreadExtra + min(thread, leftoverExtra);
-        if(thread < leftoverExtra){
-            amountPerThreadExtra++;
+    // More threads than elements in 2 rows
+    if(threadSize > elementsLeftover*2){
+        elementsPerThread++;
+        // Selects all threads with index less than width
+        if(thread < elementsLeftover){
+            calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread, width, height, thread, grid_g, threadSize);
         }
-        calc(mat_gpu, mat_gpu_tmp, amountPerThreadExtra, index_start, rows_compute, width, height, eps, maxEps, thread, grid_g);
+        // Selects all threads with index between width and width*2
+        else if(thread > elementsLeftover && thread < elementsLeftover+elementsLeftover){
+            calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread+rows_compute*(width-2), width, height, thread, grid_g, threadSize);
+        }
+    }
+    else if(threadSize > elementsLeftover){
+        elementsPerThread++;
+        if(thread < elementsLeftover){
+            // The same threads will compute both rows
+            calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread, width, height, thread, grid_g, threadSize);
+            calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread+rows_compute*(width-2), width, height, thread, grid_g, threadSize);
+        }
+    }
+    // There are less threads than elements in 1 row
+    else{
+        calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread, width, height, thread, grid_g, threadSize);
+        calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread+rows_compute*(width-2), width, height, thread, grid_g, threadSize);
+    }
+}
+
+
+
+__global__ void jacobiMid(double *mat_gpu, double *mat_gpu_tmp, int width, int height, 
+                        int rows_elementsLeftover, int device_nr, int rows_compute, int elementsPerThreadExtra, int elementsLeftoverExtra,
+                        int elementsPerThread, int elementsLeftover, int overlap_calc){
+
+
+    cg::grid_group grid_g = cg::this_grid();
+    int thread = grid_g.thread_rank();
+    int threadSize = grid_g.size();
+
+
+    if(device_nr < rows_elementsLeftover){
+        if(thread < elementsLeftoverExtra){
+            elementsPerThreadExtra++;
+        }
+        calc(mat_gpu, mat_gpu_tmp, elementsPerThreadExtra, thread+overlap_calc, width, height, thread, grid_g, threadSize);
     }
     else{
-        index_start = thread*amountPerThread + min(thread, leftover);
-        if(thread < leftover){
-            amountPerThread++;
+        if(thread < elementsLeftover){
+            elementsPerThread++;
         }
-        calc(mat_gpu, mat_gpu_tmp, amountPerThread, index_start, rows_compute, width, height, eps, maxEps, thread, grid_g);
+        calc(mat_gpu, mat_gpu_tmp, elementsPerThread, thread+overlap_calc, width, height, thread, grid_g, threadSize);
     }
-    
-    
-    
-
 }
