2e748692cee9a81be85e722356a7efc6947f3b10
Vidar Lunde Olaisen <vidaro@srl-login1.cm.cluster>
2024-03-08 16:53:51 +0100

    Updated 3d plot to contain 6 memory operations

--- fox/CPU_3d/3d_plot.py
+++ fox/CPU_3d/3d_plot.py
@@ -56,11 +56,13 @@ widths = [info[0] for info in info_list]
 heights = [info[1] for info in info_list]
 depths = [info[2] for info in info_list]
 elements = [a * b * c for a, b, c in zip(widths, heights, depths)]
-memory_operations = [2, 3, 4, 5]
+memory_operations = [2, 3, 4, 5, 6, 7]
 y_values_1 = [(memory_operations[0] * w * h * d * 8 * iterations) / bandwidth for w, h, d in zip(widths, heights, depths)]
 y_values_2 = [(memory_operations[1] * w * h * d* 8 * iterations) / bandwidth for w, h, d in zip(widths, heights, depths)]
 y_values_3 = [(memory_operations[2] * w * h * d* 8 * iterations) / bandwidth for w, h, d in zip(widths, heights, depths)]
 y_values_4 = [(memory_operations[3] * w * h * d* 8 * iterations) / bandwidth for w, h, d in zip(widths, heights, depths)]
+y_values_5 = [(memory_operations[4] * w * h * d* 8 * iterations) / bandwidth for w, h, d in zip(widths, heights, depths)]
+y_values_6 = [(memory_operations[5] * w * h * d* 8 * iterations) / bandwidth for w, h, d in zip(widths, heights, depths)]
 y_values_time = [info[5] for info in info_list]
 
 # Create plot with twin y-axis
@@ -70,12 +72,14 @@ ax2 = ax1.twinx()
 # Plot the first two lines on the left y-axis
 line1 = ax1.plot(elements, y_values_1, label='Memory Operations = 2', color='blue', marker='o')
 line2 = ax1.plot(elements, y_values_2, label='Memory Operations = 3', color='lightblue', marker='o')
-line3 = ax1.plot(elements, y_values_3, label='Memory Operations = 4', color='lightblue', marker='s')
-line4 = ax1.plot(elements, y_values_4, label='Memory Operations = 5', color='blue', marker='s')
+line3 = ax1.plot(elements, y_values_3, label='Memory Operations = 4', color='lightblue', marker='v')
+line4 = ax1.plot(elements, y_values_4, label='Memory Operations = 5', color='lightblue', marker='v')
+line5 = ax1.plot(elements, y_values_5, label='Memory Operations = 6', color='lightblue', marker='s')
+line6 = ax1.plot(elements, y_values_6, label='Memory Operations = 7', color='blue', marker='s')
 
 # Plot the third line on the right y-axis
-line5 = ax1.plot(elements, y_values_time, label='Computational Time', color='red', marker='^')
-area = ax1.fill_between(elements, y_values_1, y_values_4, color='lightgray', alpha=0.5)
+line7 = ax1.plot(elements, y_values_time, label='Computational Time', color='red', marker='^')
+area = ax1.fill_between(elements, y_values_1, y_values_6, color='lightgray', alpha=0.5)
 
 # Set labels and title for the left y-axis
 ax1.set_xlabel('Bytes')
@@ -87,7 +91,7 @@ ax2.set_ylabel('percentage execution time vs 5 memory operations', color='black'
 ax2.tick_params(axis='y', labelcolor='black')
 
 # Add legend for all lines
-lines = line1 + line2 + line3 + line4 + line5
+lines = line1 + line2 + line3 + line4 + line5 + line6 + line7
 labels = [line.get_label() for line in lines]
 ax1.legend(lines, labels, loc='upper left')
 
